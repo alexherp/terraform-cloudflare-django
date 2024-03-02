39d861800bb8c4541792cd99188f60dad9d9e1b1
Alexander <alexanderher@vmware.com>
2024-03-03 00:52:03 +0100

    iac: avoid issues when local.zones is empty

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "cloudflare_record" "record" {
   type     = each.value.type
   value    = each.value.value
   proxied  = each.value.proxied
-  zone_id  = lookup(local.zones, regex(".*?([^.]+\\.[^.]+)$", each.key)[0])
+  zone_id  = local.zones != null ? lookup(local.zones, regex(".*?([^.]+\\.[^.]+)$", each.key)[0]) : null
   lifecycle {
     ignore_changes = [zone_id]
   }
